f31a7743480846862f93e1647e5a89afec62c32e
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-04-06 13:45:16 -0600

    test completed (pre use)

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,13 +6,13 @@ namespace MyFirstUnitTests
     public class UnitTest1
     {
         [Fact]
-        public void PassingTest()
+        public void PassTestAdd()
         {
             Assert.Equal(4, Add(2, 2));
         }
 
         [Fact]
-        public void FailingTest()
+        public void FailTestAdd()
         {
             Assert.Equal(5, Add(2, 2));
         }
@@ -20,6 +20,57 @@ namespace MyFirstUnitTests
         int Add(int x, int y)
         {
             return x + y;
+        }
+
+        [Fact]
+        public void PassTestSubtract()
+        {
+            Assert.Equal(4, Subtract(8, 4));
+        }
+
+        [Fact]
+        public void FailTestSubtract()
+        {
+            Assert.Equal(4, Subtract(8, 4));
+        }
+
+        int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        [Fact]
+        public void PassTestMultiply()
+        {
+            Assert.Equal(4, Multiply(2, 2));
+        }
+
+        [Fact]
+        public void FailTestMultiply()
+        {
+            Assert.Equal(5, Multiply(2, 2));
+        }
+
+        int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        [Fact]
+        public void PassTestDivide()
+        {
+            Assert.Equal(2, Divide(4, 2));
+        }
+
+        [Fact]
+        public void FailTestDivide()
+        {
+            Assert.Equal(3, Divide(4, 2));
         }
     }
 }
