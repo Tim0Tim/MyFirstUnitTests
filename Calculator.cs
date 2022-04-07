d4fb6139d54daaa87f2631682e50061471654e77
Tim0Tim <graden.bishop2995@omail.otech.edu>
2022-04-07 11:36:10 -0600

    major changes

--- Calculator.cs
+++ Calculator.cs
@@ -4,21 +4,44 @@ namespace Calc
 {
     public class Calculator
     {
-        public Calculator()
-        {
-            //testing
-        }
-        static int TT;
-        static int stuff;
+        //int TT;
+        int x;
+        int y;
         public int Add(int x, int y)
         {
-            TT = x + y;
             return x + y;
         }
-        static void Main()
+        
+        public int Subtract(int x, int y)
         {
-            Console.WriteLine($"{TT}");
+            return x - y;
         }
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public Calculator()
+        {
+            Console.WriteLine("X  +-/*  Y");
+            Console.WriteLine("what is the first integer? (X)");
+            x = int.Parse(Console.ReadLine());
+            Console.Write($"{x}");
+            Console.WriteLine("what is the second integer? (Y)");
+            y = int.Parse(Console.ReadLine());
+            Console.Write($"{y}");
+        }
+      
+       
+       // static void Main()
+       // {
+       //     Console.WriteLine($"{TT}");
+       // }
         
     } 
 }
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -34,52 +34,43 @@ namespace MyFirstUnitTests
         [Fact]
         public void PassTestSubtract()
         {
-            Assert.Equal(4, Subtract(8, 4));
+            Calculator calc = new Calculator();
+            Assert.Equal(4, calc.Subtract(8, 4));
         }
 
         [Fact]
         public void FailTestSubtract()
         {
-            Assert.Equal(5, Subtract(8, 4));
-        }
-
-        int Subtract(int x, int y)
-        {
-            return x - y;
+            Calculator calc = new Calculator();
+            Assert.Equal(5, calc.Subtract(8, 4));
         }
 
         [Fact]
         public void PassTestMultiply()
         {
-            Assert.Equal(4, Multiply(2, 2));
+            Calculator calc = new Calculator();
+            Assert.Equal(4, calc.Multiply(2, 2));
         }
 
         [Fact]
         public void FailTestMultiply()
         {
-            Assert.Equal(5, Multiply(2, 2));
-        }
-
-        int Multiply(int x, int y)
-        {
-            return x * y;
-        }
-
-        int Divide(int x, int y)
-        {
-            return x / y;
+            Calculator calc = new Calculator();
+            Assert.Equal(5, calc.Multiply(2, 2));
         }
 
         [Fact]
         public void PassTestDivide()
         {
-            Assert.Equal(2, Divide(4, 2));
+            Calculator calc = new Calculator();
+            Assert.Equal(2, calc.Divide(4, 2));
         }
 
         [Fact]
         public void FailTestDivide()
         {
-            Assert.Equal(3, Divide(4, 2));
+            Calculator calc = new Calculator();
+            Assert.Equal(3, calc.Divide(4, 2));
         }
     }
 }
